60adc50e9c022101513564c4fe0f400869786679
okada001 <okada001@rangers.uwp.edu>
2019-06-05 17:22:21 -0500

    "cleaned up some code"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql =require("mysql2");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Renkon&daikon2363",
-    database:"todo"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_DATABASE
 });
 
 try {
